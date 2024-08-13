950f6fc980f28772cef5055ddead8fd81ae9faeb
Akshat Gupta <akshat2032003@gmail.com>
2024-08-13 17:40:34 +0530

    Switched to older engine  release to follow the tutorial

--- src/com/base/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.base;
-
-public class Constants {
-    public static final String TEXTURE_PATH = ".\\res\\textures\\";
-    public static final String MODEL_PATH = ".\\res\\models\\";
-    public static final String SHADERS_PATH = ".\\res\\shaders\\";
-}
