2e79fec05cc74dec4d990a02a17b623f08018a3b
Akshat Gupta <akshat2032003@gmail.com>
2024-08-13 17:24:36 +0530

    refactoring

--- 3DGameEngine/src/com/base/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.base;
-
-public class Constants {
-    public static final String TEXTURE_PATH = ".\\3DGameEngine\\res\\textures\\";
-    public static final String MODEL_PATH = ".\\3DGameEngine\\res\\models\\";
-    public static final String SHADERS_PATH = ".\\3DGameEngine\\res\\shaders\\";
-}
---
+++ src/com/base/Constants.java
@@ -0,0 +1,7 @@
+package com.base;
+
+public class Constants {
+    public static final String TEXTURE_PATH = ".\\res\\textures\\";
+    public static final String MODEL_PATH = ".\\res\\models\\";
+    public static final String SHADERS_PATH = ".\\res\\shaders\\";
+}
