44e456dc2a1e98cfceebd0a06df4e5f219cee317
Akshat Gupta <akshat2032003@gmail.com>
2024-08-13 14:48:23 +0530

    Initial
    Comit the game engine code from https://github.com/BennyQBD/3DGameEngine.git
    
    with some basic reasme and license files

---
+++ 3DGameEngine/src/com/base/Constants.java
@@ -0,0 +1,7 @@
+package com.base;
+
+public class Constants {
+    public static final String TEXTURE_PATH = ".\\3DGameEngine\\res\\textures\\";
+    public static final String MODEL_PATH = ".\\3DGameEngine\\res\\models\\";
+    public static final String SHADERS_PATH = ".\\3DGameEngine\\res\\shaders\\";
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Wold3DJava
+Trying to implement a wollf 3d clone in JAVA following the https://youtube.com/playlist?list=PLEETnX-uPtBV7jIbAZNCulZM1cGxJRGYs&amp;si=FRs4HBMS-t0KD0ZU playlist
